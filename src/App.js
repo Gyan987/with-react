89ddaede0a72496b21b89bd58d642f6455baa352
Gyan <gyanprakasht987@gmail.com>
2026-07-08 18:15:21 +0530

    Navbar and footer components have been added to the App.js file. The Navbar component is imported and rendered at the top of the App component. A state variable 'value' is initialized to 0, and a button is provided to increment this value when clicked. The current value is displayed in a div element.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
+  const [value,setValue]=useState(0);
   return (
+
     <div className="App">
-      Hello React!
+      <Navbar />
+      <div className="value"> {value} </div>
+      <button onClick={() => setValue(value + 1)}>Click ME </button>
+      <Footer />
     </div>
   );
 }
